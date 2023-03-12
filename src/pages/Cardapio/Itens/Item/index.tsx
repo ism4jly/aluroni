a4cba73171818de55fc2163c0988f4e1a0f648f8
ism4jly <ismaelpaulofreitas@gmail.com>
2023-03-12 15:16:57 -0300

    inserindo informacoes verdadeiras dos pratos

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,17 +1,18 @@
 import styles from './Item.module.scss';
 import logo from 'assets/logo.svg'
 import cardapio from '../itens.json';
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props){
 
-    const { title, description, category, size, serving, price} = props;
+    const { title, description, category, size, serving, price, photo} = props;
 
     return(
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt="imagem" />
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
@@ -19,7 +20,10 @@ export default function Item(props: Props){
                     <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>
+                    <div className={classNames({
+                        [styles.item__tipo]: true,
+                        [styles[`item__tipo__${category.label.toLowerCase()}`]]: true
+                    })}>
                         {category.label}
                     </div>
                     <div className={styles.item__porcao}>
